59f51dbcd771758f9362056663b95d347b08a0fd
John Shelby <jsnd11@gmail.com>
2016-12-29 18:07:17 -0500

    tropical fruit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.count <= 2
+return array.join(' and ')
+else
+  string_rep=""
+  for i in (0..array.count-2)
+string_rep << array[i]+', '
+end
+string_rep<<"and #{array[-1]}"
 
+  return string_rep
+end
 end
